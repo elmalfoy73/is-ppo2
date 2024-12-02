9992cee5c61c429d3766f49ae9ac5904ca921242
Elizabeth Malfoy <elmalfoy73@gmail.com>
2024-12-03 02:38:07 +0300

    controller

--- src/main/kotlin/ru/quipy/controller/TaskController.kt
+++ src/main/kotlin/ru/quipy/controller/TaskController.kt
@@ -1,12 +1,9 @@
 package ru.quipy.controller
 
 import org.springframework.web.bind.annotation.*
-import ru.quipy.api.TaskAggregate
-import ru.quipy.api.TaskCreatedEvent
+import ru.quipy.api.*
 import ru.quipy.core.EventSourcingService
-import ru.quipy.logic.TaskAggregateState
-import ru.quipy.logic.addTask
-import ru.quipy.logic.create
+import ru.quipy.logic.*
 import java.util.*
 
 @RestController
@@ -16,19 +13,41 @@ class TaskController(
 ) {
 
     @PostMapping("/{taskTitle}")
-    fun createTask(@PathVariable taskTitle: String, @RequestParam creatorId: String) : TaskCreatedEvent {
-        return taskEsService.create { it.create(UUID.randomUUID(), taskTitle, creatorId) }
-    }
-
-    @GetMapping("/{taskId}")
-    fun getAccount(@PathVariable taskId: UUID) : TaskAggregateState? {
-        return taskEsService.getState(taskId)
-    }
-
-    @PostMapping("/{taskId}/tasks/{taskName}")
-    fun createTask(@PathVariable taskId: UUID, @PathVariable taskName: String) : TaskCreatedEvent {
-        return taskEsService.update(taskId) {
-            it.addTask(taskName)
+    fun changeTaskName(
+        @PathVariable taskTitle: String,
+        @RequestParam userId: UUID) : TaskNameChangedEvent {
+        return taskEsService.create {
+            it.changeTaskName(taskTitle, userId)
         }
     }
+
+    @GetMapping("/{taskId}/assignTaskToUser")
+    fun getAccount(
+        @PathVariable taskId: UUID,
+        @RequestParam userId: UUID): TaskAssignedToUserEvent {
+        return taskEsService.update(taskId) {
+            it.assignTaskToUser(taskId, userId)
+        }
+    }
+
+    @PostMapping("/{taskId}/setTaskStatus")
+    fun createStatus(
+        @PathVariable taskId: UUID,
+        @RequestParam(required = true, value = "statusName") statusName: String,
+        @RequestParam(required = true, value = "userId") userId: UUID) : TaskStatusSetEvent {
+        return taskEsService.update(taskId) {
+            it.setTaskStatus(statusName, userId)
+        }
+    }
+
+    @DeleteMapping("/{taskId}/deleteStatus")
+    fun deleteStatus(
+        @PathVariable taskId: UUID,
+        @RequestParam(required = true, value = "deleterId") deleterId: UUID) : TaskStatusClearedEvent {
+        return taskEsService.update(taskId) {
+            it.clearTaskStatus(deleterId)
+        }
+    }
+
+
 }
--- src/main/kotlin/ru/quipy/controller/UserController.kt
+++ src/main/kotlin/ru/quipy/controller/UserController.kt
@@ -1,12 +1,14 @@
 package ru.quipy.controller
 
-import org.springframework.web.bind.annotation.*
-import ru.quipy.api.UserAggregate
-import ru.quipy.api.UserCreatedEvent
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import ru.quipy.api.*
 import ru.quipy.core.EventSourcingService
-import ru.quipy.logic.UserAggregateState
-import ru.quipy.logic.addUser
-import ru.quipy.logic.create
+import ru.quipy.logic.*
 import java.util.*
 
 @RestController
@@ -15,20 +17,21 @@ class UserController(
         val userEsService: EventSourcingService<UUID, UserAggregate, UserAggregateState>
 ) {
 
-    @PostMapping("/{userTitle}")
-    fun createUser(@PathVariable userTitle: String, @RequestParam creatorId: String) : UserCreatedEvent {
-        return userEsService.create { it.create(UUID.randomUUID(), userTitle, creatorId) }
+    @PostMapping("/create")
+    fun createUser(
+        @RequestParam(required = true, value = "login") login: String,
+        @RequestParam(required = true, value = "password") password: String) : UserCreatedEvent {
+        return userEsService.create {
+            it.createUser(login, password)
+        }
     }
-
-    @GetMapping("/{userId}")
-    fun getAccount(@PathVariable userId: UUID) : UserAggregateState? {
-        return userEsService.getState(userId)
-    }
-
-    @PostMapping("/{userId}/users/{userName}")
-    fun createUser(@PathVariable userId: UUID, @PathVariable userName: String) : UserCreatedEvent {
+    @PostMapping("/authorize/{userId}")
+    fun authorizeUser(
+        @PathVariable userId: UUID,
+        @RequestParam(required = true, value = "login") login: String,
+        @RequestParam(required = true, value = "password") password: String) : UserAuthorizedEvent {
         return userEsService.update(userId) {
-            it.addUser(userName)
+            it.authorizeUser(login, password)
         }
     }
 }
